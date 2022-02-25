f9c4d655719d58ef57faadb7c7f209aeda9c9c26
sz1tabist <sz1tabist@vasvari.org>
2022-02-25 11:15:33 +0100

    Feladat kész

---
+++ RectangleProject/RectangleProject/Models/Rectangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RectangleProject.Models
+{
+    public class Rectangle
+    {
+        private double a;
+        private double b;
+
+        public Rectangle(double SideA, double SideB)
+        {
+            this.a = SideA;
+            this.b = SideB;
+        }
+
+        public double SideA {
+            set
+            {
+                a = value;
+            }
+            get
+            {
+                return a;
+            }
+        }
+        public double SideB
+        {
+            set
+            {
+                b = value;
+            }
+            get
+            {
+                return b;
+            }
+        }
+
+        public double Area { get => a * b; }
+        public double Perimeter { get => 2 * (a + b); }
+
+        public override string ToString()
+        {
+            return $"Az a oldal {a}, a b oldal {b}, a téglalat kerülete: {Math.Round(Perimeter,2)}, területe: {Math.Round(Area, 2)}";
+        }
+    }
+}
--- tests/Tests.cs
+++ tests/Tests.cs
@@ -8,6 +8,8 @@ using System.IO;
 
 using RectangleProject.Models;
 
+
+
 namespace RectangleProject.Tests
 {
     [TestClass()]
